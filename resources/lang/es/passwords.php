22b791bd84a6d0632680b5c9056504f5dd938ee9
Luis Lomeli <luism.lomeli@gmail.com>
2018-03-24 18:16:39 -0700

    customize home, login and register screens

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Los passwords deben contener al menos seis caracteres y deben coincidir con la confirmación.',
+    'reset' => 'Tu password ha sido resetado.',
+    'sent' => 'Te hemos enviado una liga para resetar tu password.',
+    'token' => ' El token del password es inválido.',
+    'user' => "No se encontró un usuario con ese e-mail.",
+
+];
